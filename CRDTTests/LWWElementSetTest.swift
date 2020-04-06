f9fd0e4626aa9650387cf8541b9280ec21e8e935
Ken Siu <kensiu20042000@gmail.com>
2020-04-06 21:03:02 +0800

    add a dummy test

---
+++ CRDTTests/LWWElementSetTest.swift
@@ -0,0 +1,25 @@
+//
+//  LWWElementSetTest.swift
+//  CRDTTests
+//
+//  Created by Kam Hei Siu on 6/4/2020.
+//  Copyright © 2020 Kam Hei Siu. All rights reserved.
+//
+
+import XCTest
+
+class LWWElementSetTest: XCTestCase {
+
+    override func setUp() {
+		super.setUp()
+    }
+
+    override func tearDown() {
+		super.tearDown()
+    }
+
+    func testExample() {
+        XCTAssertTrue(1 == 1)
+    }
+
+}
